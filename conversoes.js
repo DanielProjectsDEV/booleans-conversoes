982dc7ce85d861e3225050a50604bba9db375ae6
Daniel Silveiro Alves <131623522+DanielProjectsDEV@users.noreply.github.com>
2023-11-28 22:03:35 -0300

    Add files via upload

---
+++ conversoes.js
@@ -0,0 +1,15 @@
+//conversao implicita 
+const numero = 456
+const numeroString = "456"
+
+//console.log(numero == numeroString) //true
+
+//console.log(numero + numeroString) //456456
+
+//conversao explicita
+
+//Number()
+//string()
+//conversao do tipo
+
+console.log(numero + Number(numeroString)) //912
